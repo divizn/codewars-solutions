d4a2f64931f2a9873de39c2e90c29fad9dec3b0f
Hassan <hassanjaved186@gmail.com>
2022-07-05 07:42:27 +0100

    1 6kyu done

--- src/Main.java
+++ src/Main.java
@@ -373,4 +373,34 @@ public class Main {
 
         return x == 0 && y == 0;
     }
+
+    /*
+     * (6kyu) (https://www.codewars.com/kata/54da5a58ea159efa38000836/train/java)
+     * Given an array of integers, find the one that appears an odd number of times.
+     * There will always be only one integer that appears an odd number of times.
+     * Examples:
+     * [7] should return 7, because it occurs 1 time (which is odd).
+     * [0] should return 0, because it occurs 1 time (which is odd).
+     * [1,1,2] should return 2, because it occurs 1 time (which is odd).
+     * [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+     * [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which
+     * is odd).
+     */
+    public static int findIt(int[] a) {
+        if (a.length == 1)
+            return a[0];
+
+        for (int i = 0; i < a.length; i++) {
+            int num = a[i];
+            int occurences = 0;
+            for (int j = 0; j < a.length; j++) {
+                if (a[j] == num)
+                    occurences++;
+            }
+            if (occurences % 2 != 0) {
+                return num;
+            }
+        }
+        return 0;
+    }
 }
